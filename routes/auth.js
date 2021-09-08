66a9722fff14fda9fe1fb102c1dc75619492cb04
Special K <kelian.somon@gmail.com>
2021-09-08 17:21:22 +0200

    done

---
+++ routes/auth.js
@@ -0,0 +1,82 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+const bcrypt = require("bcryptjs");
+
+
+function requireLogin(req, res, next) {
+    if (req.session.currentUser) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+}
+
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+    if (username === "" || password === "") {
+      res.render("auth/signup", { errorMessage: "Fill username and password" });
+      return;
+    }
+
+    const user = await User.findOne({ username });
+  if (user !== null) {
+    //found the user, it already exists
+    res.render("auth/signup", { errorMessage: "User already exists" });
+    return;
+  }
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  await User.create({
+    username,
+    password: hashedPassword,
+  });
+  res.redirect("/");
+});
+
+router.get("/login", (req, res) => {
+    res.render("auth/login");
+  });
+
+  router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    if (username === "" || password === "") {
+      res.render("auth/login", { errorMessage: "Fill username and password" });
+      return;
+    }
+    const user = await User.findOne({ username });
+    if (user === null) {
+      //found the user, it already exists
+      res.render("auth/login", { errorMessage: "Invalid Login" });
+      return;
+    }
+  
+    if (bcrypt.compareSync(password, user.password)) {
+      //passwords match - login successful
+      req.session.currentUser = user;
+      res.redirect("/");
+    } else {
+      res.render("auth/login", { errorMessage: "Invalid login" });
+    }
+  });
+
+  router.get("/main", (req, res) => {
+    res.render("auth/main");
+  });
+
+
+  router.get("/private", requireLogin, (req, res) => {
+    res.render("auth/private");
+  });
+
+  router.post("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+});
+
+
+  module.exports = router;
